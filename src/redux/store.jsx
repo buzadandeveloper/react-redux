54331fa02905ea301bea890296f1f176a6d810d7
Buza Dan <buzadandeveloper@gmail.com>
2024-09-22 21:17:39 +0300

    feat: created store and implemented saga

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,17 @@
 import { createStore } from "redux";
 import rootReducer from "./rootReducer";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas";
 import { applyMiddleware } from "redux";
 import { myLogger } from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(myLogger, sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
