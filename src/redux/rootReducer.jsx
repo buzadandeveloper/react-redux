b8bf83531cbc917424c3c28c447b56ea727043bd
Buza Dan <buzadandeveloper@gmail.com>
2024-09-22 21:22:43 +0300

    feat: created root for reducers and implemented middleware

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,11 +1,11 @@
-/* eslint-disable no-unused-vars */
 import { combineReducers } from "redux";
 import { toDoReducer } from "./toDoReducer";
 
 export const myLogger = (store) => (next) => (action) => {
   console.log("Logged Action:", action);
   if (action.type === "ADD_TASK") {
-    action.payload = action.payload.toUpperCase();
+    action.payload =
+      action.payload.charAt(0).toUpperCase() + action.payload.slice(1);
   }
   return next(action);
 };
