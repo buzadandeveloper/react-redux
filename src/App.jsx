5684924c10023e43fff2df026e119256e41dbb67
Buza Dan <buzadandeveloper@gmail.com>
2024-09-22 21:26:32 +0300

    style: format code with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ToDoList } from "./components/ToDoList/ToDoList";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+
 function App() {
   return (
     <Provider store={store}>
